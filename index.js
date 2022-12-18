c00fd909e7c4617ce823f2af8b74797b4e63d53f
unknown <2191820@my.ipleiria.pt>
2022-12-18 03:58:38 +0000

    newitem socket

--- index.js
+++ index.js
@@ -8,22 +8,11 @@ const io = require("socket.io")(httpServer, {
         credentials: true
     }
 })
-httpServer.listen(8080, () =>{
+httpServer.listen(8080, () => {
     console.log('listening on *:8080')
 })
 io.on('connection', (socket) => {
-    /*
-    console.log(`client ${socket.id} has connected`)
-    socket.on('newProject', (project) => {
-        socket.broadcast.emit('newProject', project)
-    })
-    socket.on('updateProject', (project) => {
-        socket.broadcast.emit('updateProject', project)
-    })
-    socket.on('deleteProject', (project) => {
-        socket.broadcast.emit('deleteProject', project)
-    })
-      */
+
     socket.on('loggedIn', function (user) {
         socket.join(user.id)
         if (user.type == 'EM') {
@@ -33,11 +22,12 @@ io.on('connection', (socket) => {
             socket.join('Chef')
         }
     })
+
     socket.on('loggedOut', function (user) {
         socket.leave(user.id)
         socket.leave('administrator')
     })
-  
+
     socket.on('updateUser', function (user) {
         socket.in('Manager').except(user.id).emit('updateUser', user)
         socket.in(user.id).emit('updateUser', user)
@@ -45,6 +35,10 @@ io.on('connection', (socket) => {
 
     socket.on('updateItem', function (item) {
         socket.in('Chef').emit('updateItem', item)
+    })
+
+    socket.on('newItem', function (item) {
+        socket.in('Chef').emit('newItem', item)
     })
 
 })
